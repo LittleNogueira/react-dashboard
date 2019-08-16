d7c62326b88a9466b45a079698e7eafed4ed462f
mateus.oliveira@itbam.org.br <you@example.com>
2019-08-16 16:42:40 -0400

    Add bars and remove responsive

--- src/components/topnavbar/TopNavbar.js
+++ src/components/topnavbar/TopNavbar.js
@@ -2,10 +2,19 @@ import React,{Component} from 'react';
 
 import './TopNavbar.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/fontawesome-free-solid'
+
 class TopNavbar extends Component{
     render(){
         return(
             <div className="container-topnavbar" >
+                <div className="bars" >
+                    <FontAwesomeIcon icon={faBars} />
+                </div>
+                <ul className="options-topnavbar" >
+
+                </ul>
                 {/* <div className="content-topnavbar" >
                     <p>Cloud four - Productions</p>
                 </div>
